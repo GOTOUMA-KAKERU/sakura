445f1324fa6ddeeecd776ec2fec2f12c25a486fd
KAKERU <43anillo@gmail.com>
2025-02-03 21:00:32 +0000

    a

--- others/script.js
+++ others/script.js
@@ -1,4 +1,6 @@
 function con_detail(element){
+    var contact_detail = document.getElementById('contact_server_background');
+    contact_detail.style.display = "grid";
     var jsonname = "./contact_servers/jsons/" + element.id + ".json";
     fetch(jsonname) // JSON を取得
     .then(response => response.json()) // JSON をオブジェクトに変換
@@ -10,4 +12,10 @@ function con_detail(element){
     })
     .catch(error => console.error("エラー:", error));
 
+}
+
+function back(){
+    console.log("clicked");
+    var contact_detail = document.getElementById('contact_server_background');
+    contact_detail.style.display = "none";
 }
